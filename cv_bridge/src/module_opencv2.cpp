9f468f894aae1efeef2c9f1f8d68102b11816588
Jiwoong Choi <jiw8ng.choi@gmail.com>
2020-02-13 19:29:11 +0900

    Compile against OpenCV 4 that comes with JetPack 4.3

--- cv_bridge/src/module_opencv2.cpp
+++ cv_bridge/src/module_opencv2.cpp
@@ -95,59 +95,77 @@ static inline int* refcountFromPyObject( const PyObject* obj )
 return ( int* )(( size_t )obj + REFCOUNT_OFFSET );
 }
 
-class NumpyAllocator : public cv::MatAllocator
+class NumpyAllocator : public MatAllocator
 {
 public:
-NumpyAllocator( ) { }
-~NumpyAllocator( ) { }
+    NumpyAllocator() { stdAllocator = Mat::getStdAllocator(); }
+    ~NumpyAllocator() {}
 
-void allocate( int dims, const int* sizes, int type, int*& refcount,
-uchar*& datastart, uchar*& data, size_t* step );
+    UMatData* allocate(PyObject* o, int dims, const int* sizes, int type, size_t* step) const
+    {
+        UMatData* u = new UMatData(this);
+        u->data = u->origdata = (uchar*)PyArray_DATA((PyArrayObject*) o);
+        npy_intp* _strides = PyArray_STRIDES((PyArrayObject*) o);
+        for( int i = 0; i < dims - 1; i++ )
+            step[i] = (size_t)_strides[i];
+        step[dims-1] = CV_ELEM_SIZE(type);
+        u->size = sizes[0]*step[0];
+        u->userdata = o;
+        return u;
+    }
 
-void deallocate( int* refcount, uchar* datastart, uchar* data );
+    UMatData* allocate(int dims0, const int* sizes, int type, void* data, size_t* step, AccessFlag flags, UMatUsageFlags usageFlags) const CV_OVERRIDE
+    {
+        if( data != 0 )
+        {
+            // issue #6969: CV_Error(Error::StsAssert, "The data should normally be NULL!");
+            // probably this is safe to do in such extreme case
+            return stdAllocator->allocate(dims0, sizes, type, data, step, flags, usageFlags);
+        }
+        // PyEnsureGIL gil;
+
+        int depth = CV_MAT_DEPTH(type);
+        int cn = CV_MAT_CN(type);
+        const int f = (int)(sizeof(size_t)/8);
+        int typenum = depth == CV_8U ? NPY_UBYTE : depth == CV_8S ? NPY_BYTE :
+        depth == CV_16U ? NPY_USHORT : depth == CV_16S ? NPY_SHORT :
+        depth == CV_32S ? NPY_INT : depth == CV_32F ? NPY_FLOAT :
+        depth == CV_64F ? NPY_DOUBLE : f*NPY_ULONGLONG + (f^1)*NPY_UINT;
+        int i, dims = dims0;
+        cv::AutoBuffer<npy_intp> _sizes(dims + 1);
+        for( i = 0; i < dims; i++ )
+            _sizes[i] = sizes[i];
+        if( cn > 1 )
+            _sizes[dims++] = cn;
+        PyObject* o = PyArray_SimpleNew(dims, _sizes.data(), typenum);
+        if(!o)
+            CV_Error_(Error::StsError, ("The numpy array of typenum=%d, ndims=%d can not be created", typenum, dims));
+        return allocate(o, dims0, sizes, type, step);
+    }
+
+    bool allocate(UMatData* u, AccessFlag accessFlags, UMatUsageFlags usageFlags) const CV_OVERRIDE
+    {
+        return stdAllocator->allocate(u, accessFlags, usageFlags);
+    }
+
+    void deallocate(UMatData* u) const CV_OVERRIDE
+    {
+        if(!u)
+            return;
+        // PyEnsureGIL gil;
+        CV_Assert(u->urefcount >= 0);
+        CV_Assert(u->refcount >= 0);
+        if(u->refcount == 0)
+        {
+            PyObject* o = (PyObject*)u->userdata;
+            Py_XDECREF(o);
+            delete u;
+        }
+    }
+
+    const MatAllocator* stdAllocator;
 };
 
-void NumpyAllocator::allocate( int dims, const int* sizes, int type, int*& refcount, uchar*& datastart, uchar*& data, size_t* step )
-{
-    int depth = CV_MAT_DEPTH( type );
-    int cn = CV_MAT_CN( type );
-    const int f = ( int )( sizeof( size_t )/8 );
-    int typenum = depth == CV_8U ? NPY_UBYTE : depth == CV_8S ? NPY_BYTE :
-                  depth == CV_16U ? NPY_USHORT : depth == CV_16S ? NPY_SHORT :
-                  depth == CV_32S ? NPY_INT : depth == CV_32F ? NPY_FLOAT :
-                  depth == CV_64F ? NPY_DOUBLE : f*NPY_ULONGLONG + (f^1)*NPY_UINT;
-    int i;
-    npy_intp _sizes[CV_MAX_DIM+1];
-    for( i = 0; i < dims; i++ )
-        _sizes[i] = sizes[i];
-    if( cn > 1 )
-    {
-    /*if( _sizes[dims-1] == 1 )
-         _sizes[dims-1] = cn;
-    else*/
-        _sizes[dims++] = cn;
-    }
-    PyObject* o = PyArray_SimpleNew( dims, _sizes, typenum );
-    if( !o )
-    CV_Error_(CV_StsError, ("The numpy array of typenum=%d, ndims=%d can not be created", typenum, dims));
-    refcount = refcountFromPyObject(o);
-    npy_intp* _strides = PyArray_STRIDES((PyArrayObject*) o);
-    for( i = 0; i < dims - (cn > 1); i++ )
-        step[i] = (size_t)_strides[i];
-    datastart = data = (uchar*)PyArray_DATA((PyArrayObject*)o);
-
-}
-
-void NumpyAllocator::deallocate( int* refcount, uchar* datastart, uchar* data )
-{
-    if( !refcount )
-       return;
-    PyObject* o = pyObjectFromRefcount(refcount);
-    Py_INCREF(o);
-    Py_DECREF(o);
-}
-
-// Declare the object
 NumpyAllocator g_numpyAllocator;
 
 int convert_to_CvMat2(const PyObject* o, cv::Mat& m)
@@ -230,7 +248,7 @@ int convert_to_CvMat2(const PyObject* o, cv::Mat& m)
 
     if( m.data )
     {
-        m.refcount = refcountFromPyObject(o);
+        m.u->refcount = *refcountFromPyObject(o);
         m.addref(); // protect the original numpy array from deallocation
         // (since Mat destructor will decrement the reference counter)
     };
@@ -251,12 +269,12 @@ PyObject* pyopencv_from(const Mat& m)
     if( !m.data )
         Py_RETURN_NONE;
     Mat temp, *p = (Mat*)&m;
-    if(!p->refcount || p->allocator != &g_numpyAllocator)
+    if(!p->u->refcount || p->allocator != &g_numpyAllocator)
     {
         temp.allocator = &g_numpyAllocator;
         ERRWRAP2(m.copyTo(temp));
         p = &temp;
     }
     p->addref();
-    return pyObjectFromRefcount(p->refcount);
+    return pyObjectFromRefcount(&p->u->refcount);
 }
